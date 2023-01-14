bf54928948471babddfe453b783e5ced520b6325
Zihad07 <zihadnayem8@gmail.com>
2023-01-14 18:25:27 +0600

    learning basic Routing.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return "Contact Image";
+})->name('home.contact');
+Route::get('/about', function () {
+    return "About Page";
+})->name('home.about');
+
+// parameter
+// Route::get('/post/{id}', function ($id) {
+//     return "Post ID: " . $id;
+// })->name('post.show');
+
+
+// with regular expresion
+Route::get('/post/{id}', function ($id) {
+    return "Post ID: " . $id;
+})->where([
+    'id' => '[0-9]+'
+])
+    ->name('post.show');
+
+// optionnal parameter
+Route::get('/recent-post/{days_ago?}', function ($days_ago = 3) {
+    return "Recent Post " . $days_ago . ' ago.';
+})->name('post.show');
